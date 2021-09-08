b24ffc31f0d93f31e06e2bac23d260d604aa2731
Domicles <37049614+Dominiq97@users.noreply.github.com>
2021-09-08 14:49:53 +0300

    key

--- symptom-checker/symptom_checker/settings.py
+++ symptom-checker/symptom_checker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's3c8ftg-+bosy!4iw@31l=a3bt&++lz7+!60@3)t5o-@arh20&'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
